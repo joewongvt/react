8b1f3e6398202bb631be6fa3b10a40f4fd91ad30
Joshua Wang <jowang1@vt.edu>
2015-04-24 23:38:36 -0400

    everything working except indirect references to a cyclical dependency

--- 02-calculator/src/main/scala/calculator/Calculator.scala
+++ 02-calculator/src/main/scala/calculator/Calculator.scala
@@ -9,20 +9,32 @@ final case class Times(a: Expr, b: Expr) extends Expr
 final case class Divide(a: Expr, b: Expr) extends Expr
 
 object Calculator {
-  def computeValues(
-      namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
-    ???
+  def computeValues(namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
+    namedExpressions.mapValues( e => Signal(eval(e(), namedExpressions.filter( (kv:(String, Signal[Expr])) => kv._2 != e ))))
   }
 
   def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
-    ???
+    print(s"expr: ${expr}, references: ")
+    references.foreach { case (key, value) => print(s"${key}, ") }
+    println("")
+    expr match {
+      case l:Literal => l.v
+      case r:Ref     => eval(getReferenceExpr(r.name, references), references)
+      case p:Plus    => eval(p.a, references) + eval(p.b, references)
+      case m:Minus   => eval(m.a, references) - eval(m.b, references)
+      case t:Times   => eval(t.a, references) * eval(t.b, references)
+      case d:Divide  => eval(d.a, references) / eval(d.b, references)
+      case _         => Double.NaN
+    }
   }
 
   /** Get the Expr for a referenced variables.
    *  If the variable is not known, returns a literal NaN.
    */
-  private def getReferenceExpr(name: String,
-      references: Map[String, Signal[Expr]]) = {
+  private def getReferenceExpr(name: String, references: Map[String, Signal[Expr]]) = {
+    print("getReferenceExpr: ")
+    references.foreach { case (key, value) => print(s"$key, ")}
+    println("")
     references.get(name).fold[Expr] {
       Literal(Double.NaN)
     } { exprSignal =>
