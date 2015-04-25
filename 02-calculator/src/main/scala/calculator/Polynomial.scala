aed3c1e250ca7b5ea16d4e34b27099a4ec492475
Joshua Wang <jowang1@vt.edu>
2015-04-24 21:38:54 -0400

    polynomial calculator

--- 02-calculator/src/main/scala/calculator/Polynomial.scala
+++ 02-calculator/src/main/scala/calculator/Polynomial.scala
@@ -3,11 +3,18 @@ package calculator
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] = {
-    ???
+    Signal(b.apply()*b.apply() - 4*a.apply()*c.apply())
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+
+    Signal(
+      computeDelta(a,b,c).apply() match {
+        case imaginary if imaginary < 0 => Set()
+        case single if single == 0 => Set(-b.apply()/(2*a.apply()))
+        case _ => Set( (-b.apply() + Math.sqrt(computeDelta(a,b,c).apply()))/(2*a.apply()) , (-b.apply() - Math.sqrt(computeDelta(a,b,c).apply()))/(2*a.apply()))
+      }
+    )
   }
 }
