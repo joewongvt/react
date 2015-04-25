1c4b7e405688938a474b1a3a91ae822d6d3d92af
Joshua Wang <jowang1@vt.edu>
2015-04-24 23:51:49 -0400

    correct answers, unnecessary recomputation

--- 02-calculator/src/main/scala/calculator/Calculator.scala
+++ 02-calculator/src/main/scala/calculator/Calculator.scala
@@ -14,27 +14,25 @@ object Calculator {
   }
 
   def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
-    print(s"expr: ${expr}, references: ")
-    references.foreach { case (key, value) => print(s"${key}, ") }
-    println("")
     expr match {
       case l:Literal => l.v
       case r:Ref     => eval(getReferenceExpr(r.name, references), references)
-      case p:Plus    => eval(p.a, references) + eval(p.b, references)
-      case m:Minus   => eval(m.a, references) - eval(m.b, references)
-      case t:Times   => eval(t.a, references) * eval(t.b, references)
-      case d:Divide  => eval(d.a, references) / eval(d.b, references)
+      case p:Plus    => eval(p.a, filterOut(p, references)) + eval(p.b, filterOut(p, references))
+      case m:Minus   => eval(m.a, filterOut(m, references)) - eval(m.b, filterOut(m, references))
+      case t:Times   => eval(t.a, filterOut(t, references)) * eval(t.b, filterOut(t, references))
+      case d:Divide  => eval(d.a, filterOut(d, references)) / eval(d.b, filterOut(d, references))
       case _         => Double.NaN
     }
+  }
+
+  def filterOut(e:Expr, references: Map[String, Signal[Expr]]): Map[String, Signal[Expr]] = {
+    references.filter( (kv:(String, Signal[Expr])) => kv._2() != e)
   }
 
   /** Get the Expr for a referenced variables.
    *  If the variable is not known, returns a literal NaN.
    */
   private def getReferenceExpr(name: String, references: Map[String, Signal[Expr]]) = {
-    print("getReferenceExpr: ")
-    references.foreach { case (key, value) => print(s"$key, ")}
-    println("")
     references.get(name).fold[Expr] {
       Literal(Double.NaN)
     } { exprSignal =>
